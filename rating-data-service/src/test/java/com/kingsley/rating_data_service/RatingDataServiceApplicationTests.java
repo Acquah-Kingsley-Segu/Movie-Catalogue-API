0ca9c9c0df47ea826e70ce3d70a6868905f84153
Kingsley Acquah <kingsley.acquah@amalitech.com>
2024-06-21 09:45:42 +0000

    The microservice responsible for rating a user's movie watched

---
+++ rating-data-service/src/test/java/com/kingsley/rating_data_service/RatingDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kingsley.rating_data_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
