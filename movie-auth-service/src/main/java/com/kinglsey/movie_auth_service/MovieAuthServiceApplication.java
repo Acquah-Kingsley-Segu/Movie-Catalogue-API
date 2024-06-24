72813eac08b6028de17fbe72486079c7fe62c37c
Kingsley Acquah <kingsley.acquah@amalitech.com>
2024-06-24 12:48:07 +0000

    App security microservice

---
+++ movie-auth-service/src/main/java/com/kinglsey/movie_auth_service/MovieAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.kinglsey.movie_auth_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieAuthServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieAuthServiceApplication.class, args);
+	}
+
+}
---
+++ movie-auth-service/src/test/java/com/kinglsey/movie_auth_service/MovieAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kinglsey.movie_auth_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieAuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
