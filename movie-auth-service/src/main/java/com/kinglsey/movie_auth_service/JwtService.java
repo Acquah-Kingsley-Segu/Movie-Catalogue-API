e13b711c8af0b53109c9ae05afa23ca80c11fa11
KingsleySeguAcquah <acquahkingsleysegu10@gmail.com>
2024-07-11 14:57:45 +0000

    feat/properties-segregation: different settings value are set for different environment

--- movie-auth-service/src/main/java/com/kinglsey/movie_auth_service/JwtService.java
+++ movie-auth-service/src/main/java/com/kinglsey/movie_auth_service/JwtService.java
@@ -4,6 +4,7 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 
@@ -15,7 +16,8 @@ import java.util.function.Function;
 
 @Component
 public class JwtService {
-    private static final String SECRET = "3e216cb1bba90db0a0aafff3be3b03002c5c3b9a59b17d347a7312b485e1329db628750d91811f4f73dc2ea73213558131c7beb1e26ca11fe946e3ccaec7b687e8a4bb70ab09f3dfc438cb60abc922aab45d3b9b334bee546d7fbac14ada59e6c36d4e72525e2a45898cf2cf87f902fc2c8b55c85c42b3a6539da6637fbb476a0133a0f704e538d9d61d4815e4abae0741e12f0376c80868b7c6b2ae68828389bb4a01adcc96114517f4b958475e39c52c7d140e1b5f74ab313108878a0c5fbdec6319c8a59c1cb09c34595b32292bc2467ead7b6e20688600a6799f01ea4af18af42b6bc1f0aeda5d61285e777f5493b41d1740ef6c646f9133074c01b2b8e6";
+    @Value("${JWT_SECRET}")
+    private String secret;
 
     public String extractUsername(String token) {
         return extractClaim(token, Claims::getSubject);
@@ -39,7 +41,7 @@ public class JwtService {
     }
 
     private Key getSigningKey() {
-        byte[] keyBytes = SECRET.getBytes();
+        byte[] keyBytes = secret.getBytes();
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
