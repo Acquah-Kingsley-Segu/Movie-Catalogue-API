33e7239fc10a796fea5f8601041f4c17279bbb86
KingsleySeguAcquah <acquahkingsleysegu10@gmail.com>
2024-09-09 22:51:55 +0000

    feat/config-server: made it a config client

--- movie-auth-service/src/main/java/com/kinglsey/movie_auth_service/MovieAuthServiceApplication.java
+++ movie-auth-service/src/main/java/com/kinglsey/movie_auth_service/MovieAuthServiceApplication.java
@@ -1,7 +1,11 @@
 package com.kinglsey.movie_auth_service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class MovieAuthServiceApplication {
@@ -10,4 +14,15 @@ public class MovieAuthServiceApplication {
 		SpringApplication.run(MovieAuthServiceApplication.class, args);
 	}
 
+	@RestController
+	@RefreshScope
+	static class MovieAuthController {
+		@Value("${spring.datasource.url}")
+		private String dbUrl;
+
+		@GetMapping("/dbUrl")
+		public String dbUrl() {
+			return dbUrl;
+		}
+	}
 }
